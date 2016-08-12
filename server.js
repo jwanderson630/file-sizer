0ab0936598ffe7dd7f6d9e56c4e2a5135e82d3dd
Joe Anderson <yoej262@gmail.com>
2016-08-12 22:54:10 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var imageSearch = require('node-google-image-search');
+var history = [];
+app.get('/search/:TERMS', function(req, res) {
+  var results = imageSearch(req.params.TERMS, function(final){
+      res.send(final);
+      history.push(req.params.TERMS);
+    }, req.query.offset, 5);
+});
+app.get('/latest', function(req,res){
+  res.send(history);
+});
+app.listen(8080, function () {
+  console.log('Image Search listening on port 8080');
+});
